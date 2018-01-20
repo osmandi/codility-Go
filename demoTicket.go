b0ac11e6b03d85056b460c4d29e01e60bdcd2a58
osmandi <osmandi.github@gmail.com>
2018-01-20 14:19:01 -0400

    Terminando demoTicket

--- demoTicket.go
+++ demoTicket.go
@@ -1,17 +1,24 @@
 package main
 
+/*
+Demo ticket:
+Correctness= 100%
+Performance= 100%
+
+Test score= 100%
+*/
 import (
 	"fmt"
 	"sort"
 )
 
+// Elimina los valores repetdos y el cero
 func repetidos(A []int) []int {
 
-	// Eliminar elementos repetidos
 	encontrado := make(map[int]bool)
 	arrayModificado := []int{}
 	for i := range A {
-		if encontrado[A[i]] == false {
+		if encontrado[A[i]] == false && A[i] > 0 {
 			encontrado[A[i]] = true
 			arrayModificado = append(arrayModificado, A[i])
 		}
@@ -20,17 +27,13 @@ func repetidos(A []int) []int {
 	return arrayModificado
 }
 
-func Solution(A []int) (int, []int) {
+func Solution(A []int) int {
 
-	// Ordenar array de mayo a menor
+	// Ordenar array de mayor a menor
 	sort.Ints(A)
 
+	// Limpiar datos
 	arrayModificado := repetidos(A)
-
-	// Determinar el resultado
-	// 	Si son negativos: 1
-	// 	Retornar el menor valor faltante
-	// 	En una secuencia retornar el sigueinte último valor
 
 	var resultado int = 0
 	for i := range arrayModificado {
@@ -39,35 +42,39 @@ func Solution(A []int) (int, []int) {
 		if arrayModificado[i] > 0 && arrayModificado[i] == resultado+1 {
 			resultado += 1
 		} else {
-			return resultado + 1, arrayModificado
+			return resultado + 1
 		}
 
 		// Para valores secuenciales
 		if arrayModificado[i] > 0 && i == len(arrayModificado)-1 {
-			return arrayModificado[i] + 1, arrayModificado
+			return arrayModificado[i] + 1
 		}
 	}
 
 	// Para valores negativos
-	return 1, arrayModificado
+	return 1
 }
 
+// Función main y definición de casos
 func main() {
-	A := []int{1, 2, 3}
+	A := []int{2, 3, 4}
 	B := []int{1, 3, 6, 4, 1, 2}
 	C := []int{1, 2, 3, 4, 5, 9}
 	D := []int{-1, -3}
 	E := []int{}
-	for i := 0; i <= 100000; i++ {
+	for i := 1; i <= 2000000; i++ {
 		E = append(E, i)
-		fmt.Println(E[i])
 	}
+
+	E = append(E[:132498], E[132857:]...)
+
+	//fmt.Println(E)
 
 	tests := [][]int{A, B, C, D, E}
 
 	for i := range tests {
-		resultado, array := Solution(tests[i])
-		fmt.Println(resultado, array)
+		resultado := Solution(tests[i])
+		fmt.Println(resultado)
 	}
 
 }
