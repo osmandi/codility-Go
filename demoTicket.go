87650556780bdc8d86d602bc412a4805426976e8
osmandi <osmandi.github@gmail.com>
2018-01-20 15:14:20 -0400

    Agregando descripción del ejercicio

--- demoTicket.go
+++ demoTicket.go
@@ -1,18 +1,49 @@
 package main
 
+// Estado:
 /*
-Demo ticket:
-Correctness= 100%
-Performance= 100%
+	Demo ticket:
+	Correctness= 100%
+	Performance= 100%
 
-Test score= 100%
+	Test score= 100%
+
+	En codility no se incluye la función main
 */
+
+// Ejercicio
+
+/*
+This is a demo task.
+
+Write a function:
+
+func Solution(A []int) int
+
+that, given an array A of N integers, returns the smallest positive integer (greater than 0) that does not occur in A.
+
+For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5.
+
+Given A = [1, 2, 3], the function should return 4.
+
+Given A = [−1, −3], the function should return 1.
+
+Assume that:
+
+N is an integer within the range [1..100,000];
+each element of array A is an integer within the range [−1,000,000..1,000,000].
+Complexity:
+
+expected worst-case time complexity is O(N);
+expected worst-case space complexity is O(N), beyond input storage (not counting the storage required for input arguments).
+*/
+
 import (
 	"fmt"
 	"sort"
 )
 
-// Elimina los valores repetdos y el cero
+// Elimina los valores repetidos y el cero
 func repetidos(A []int) []int {
 
 	encontrado := make(map[int]bool)
