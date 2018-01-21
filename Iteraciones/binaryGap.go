39eeb2a65c0f6949605b4efc44dfa19a9cb155c5
osmandi <osmandi.github@gmail.com>
2018-01-20 20:40:47 -0400

    Agreganto Iteraaciones

---
+++ Iteraciones/binaryGap.go
@@ -0,0 +1,31 @@
+// Notas
+/*
+- Retornar la longitud máxima de un número binario (cantidad de 0 entre 1) Ejemplo:
+	9 -> 1001 -> Longitud= 2
+	529 -> 1000010001 -> Longitud= 4 y 3
+	15 -> 1111 -> Longitud= 0
+
+- Para convertir un entero a binario, dividir entre dos y hasta que dé uno se tomará el último cociente y los residuos de forma ascendente
+
+- En Golang para convertir un número decimal a binario:
+	strconv.FormatInt(int64(numero), 2)
+
+- Para saber el tipo de variable usar reflect.TypeOf(variable)
+
+- Para convertir string a int usar strconv.Atoi("string")
+
+- Para convertir int a string usar Z,_ :=strconv.Itoa(int)
+*/
+
+package main
+
+// Convertir decimal a Binario
+func decimalToBinary(N int) int {
+
+}
+
+func Solution(N int) int {
+}
+
+func main() {
+}
